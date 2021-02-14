8b3f9188f60225f99a5bc5c6d680b619763e869a
Casey Milne <casey.milne@eatbuildplay.com>
2021-02-14 06:11:09 -0500

    load poppins font

--- themes/showcase/js/search.js
+++ themes/showcase/js/search.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
 
+  // Bedrooms Field
   var bedroomSelectOptions = [
     {
       value: "0",
@@ -26,6 +27,31 @@ jQuery(document).ready(function($) {
   $("#field-bedrooms").select({
     label: "Bedrooms",
     items: bedroomSelectOptions
+  });
+
+  // Bathrooms Field
+  var bathroomSelectOptions = [
+    {
+      value: "1",
+      title: "1+"
+    },
+    {
+      value: "2",
+      title: "2+"
+    },
+    {
+      value: "3",
+      title: "3+"
+    },
+    {
+      value: "4",
+      title: "4+"
+    }
+  ];
+
+  $("#field-bathrooms").select({
+    label: "Bathrooms",
+    items: bathroomSelectOptions
   });
 
 });
